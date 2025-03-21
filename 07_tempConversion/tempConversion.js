92db28d2d061a8b1ebc1ec3a630d4191cf90e02e
Briscoe Kerferd <bjkerferd@pm.me>
2025-03-22 08:36:57 +1030

    Complete 07-tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  tempC = (tempF - 32) * 5/9; // Refer Wikipedia for formula 
+  return Math.round(tempC * 10) / 10; // Return val to 1 d.p.
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  tempF = (tempC * 9/5 + 32); 
+  return Math.round(tempF * 10) / 10; // Return val to 1 d.p
 };
 
 // Do not edit below this line
