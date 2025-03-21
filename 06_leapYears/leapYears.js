a5eb8326f61704eddb404137b43e672d17456079
Briscoe Kerferd <bjkerferd@pm.me>
2025-03-22 08:29:27 +1030

    Complete 06-leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const isYearDivisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isQuadCentury = year % 400 === 0;
+    return (isYearDivisibleByFour && (!isCentury || isQuadCentury));
 };
 
 // Do not edit below this line
